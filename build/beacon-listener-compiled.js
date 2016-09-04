976b45b18375ae08d921252883e04c68b753e24c
Kris Kelly <bertpotato@gmail.com>
2016-09-04 02:29:44 +0100

    added array polyfill moved babel helpers to external file

--- build/beacon-listener-compiled.js
+++ build/beacon-listener-compiled.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 var TOP = 'top';
 var RIGHT = 'right';
 var BOTTOM = 'bottom';
@@ -17,7 +11,7 @@ var EVENT_TYPE_BEACON_LOST = 'beacon-listener:lost';
  */
 
 var BeaconListener = function () {
-	_createClass(BeaconListener, [{
+	babelHelpers.createClass(BeaconListener, [{
 		key: 'events',
 		get: function get() {
 			return me._events;
@@ -25,7 +19,8 @@ var BeaconListener = function () {
 	}]);
 
 	function BeaconListener(config) {
-		_classCallCheck(this, BeaconListener);
+		babelHelpers.classCallCheck(this, BeaconListener);
+
 
 		var me = this,
 		    staticClass = BeaconListener,
@@ -88,7 +83,7 @@ var BeaconListener = function () {
   *****************/
 
 
-	_createClass(BeaconListener, [{
+	babelHelpers.createClass(BeaconListener, [{
 		key: 'stop',
 		value: function stop() {
 			var me = this;
@@ -503,7 +498,7 @@ var BeaconListener = function () {
 	}, {
 		key: 'isObject',
 		value: function isObject(obj) {
-			var varType = typeof obj === 'undefined' ? 'undefined' : _typeof(obj),
+			var varType = typeof obj === 'undefined' ? 'undefined' : babelHelpers.typeof(obj),
 			    staticClass = BeaconListener;
 			return obj && (varType === 'object' || varType === 'function' || staticClass.isFunction(obj)) || false;
 		}
@@ -746,7 +741,6 @@ var BeaconListener = function () {
 			return (((t = doc.documentElement) || (t = doc.body.parentNode)) && typeof t.scrollTop == 'number' ? t : doc.body).scrollTop;
 		}
 	}]);
-
 	return BeaconListener;
 }();
 
