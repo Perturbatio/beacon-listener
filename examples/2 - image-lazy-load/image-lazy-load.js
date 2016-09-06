3ddb6df8e20a79b8b6fdd1a41e722f6c85c8b0be
Kris Kelly <bertpotato@gmail.com>
2016-09-07 00:20:14 +0100

    wrapped image lazy load code in domcontentready event

--- examples/2 - image-lazy-load/image-lazy-load.js
+++ examples/2 - image-lazy-load/image-lazy-load.js
@@ -2,20 +2,23 @@
  * Created by kris on 03/09/16.
  */
 (function () {
-	var imageListener = new BeaconListener({
-		beacons     : 'img[data-src]',
-		fullyInside : true,
-		pollInterval: 1000 / 60,
-		autoStart: true
-	});
 
-	imageListener.addEventListener('beacon-listener:found', function(e){
-		var img  = e.detail.beacon,
-			newSrc = img.getAttribute('data-src');
+	document.addEventListener("DOMContentLoaded", function ( event ) {
+		var imageListener = new BeaconListener({
+			beacons     : 'img[data-src]',
+			fullyInside : false,
+			pollInterval: 1000 / 60,
+			autoStart   : true
+		});
 
-		if (newSrc && newSrc.length > 0){
-			img.src = newSrc;
-			img.removeAttribute('data-src');
-		}
+		imageListener.addEventListener('beacon-listener:found', function ( e ) {
+			var img = e.detail.beacon,
+				newSrc = img.getAttribute('data-src');
+
+			if ( newSrc && newSrc.length > 0 ) {
+				img.src = newSrc;
+				img.removeAttribute('data-src');
+			}
+		});
 	});
 })();
